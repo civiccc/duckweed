25f40b985199d5316d247d6c47c66634385c7e91
Greg Hurrell <greg@causes.com>
2011-05-05 20:19:43 -0700

    Divide specs into "not seen" and "seen" events

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,39 +11,75 @@ describe Duckweed::App do
   end
 
   describe "POST /track/:event" do
+    let(:event) { 'test-event-47781' }
+
     before do
       @now = Time.now
       Time.stub!(:now).and_return(@now)
     end
 
-    it "accepts requests for new events" do
-      post "/track/test-event-71339"
-      last_response.should be_successful
-      last_response.body.should == "OK"
+    context 'with a new event' do
+      it 'succeeds' do
+        post "/track/#{event}"
+        last_response.should be_successful
+      end
+
+      it 'responds with "OK"' do
+        post "/track/#{event}"
+        last_response.body.should == "OK"
+      end
+
+      it "increments a key with minute-granularity in Redis" do
+        expect { post "/track/#{event}" }.to change {
+          Duckweed.redis.get("duckweed:#{event}:minutes:#{@now.to_i / 60}")
+        }.from(nil).to(1)
+      end
+
+      it "increments a key with hour-granularity in Redis" do
+        expect { post "/track/#{event}" }.to change {
+          Duckweed.redis.get("duckweed:#{event}:hours:#{@now.to_i / 3600}")
+        }.from(nil).to(1)
+      end
+
+      it "increments a key with day-granularity in Redis" do
+        expect { post "/track/#{event}" }.to change {
+          Duckweed.redis.get("duckweed:#{event}:days:#{@now.to_i / 86400}")
+        }.from(nil).to(1)
+      end
     end
 
-    it "accepts requests for known events" do
-      2.times { post "/track/test-event-47781" }
-      last_response.should be_successful
-      last_response.body.should == "OK"
-    end
+    context 'with a previously-seen event' do
+      before do
+        post "/track/#{event}"
+      end
 
-    it "increments a key with minute-granularity in Redis" do
-      expect { post "/track/test-incr" }.to change {
-        Duckweed.redis.get("duckweed:test-incr:minutes:#{@now.to_i / 60}")
-      }.from(nil).to(1)
-    end
+      it 'succeeds' do
+        post "/track/#{event}"
+        last_response.should be_successful
+      end
 
-    it "increments a key with hour-granularity in Redis" do
-      expect { post "/track/test-incr" }.to change {
-        Duckweed.redis.get("duckweed:test-incr:hours:#{@now.to_i / 3600}")
-      }.from(nil).to(1)
-    end
+      it 'responds with "OK"' do
+        post "/track/#{event}"
+        last_response.body.should == "OK"
+      end
 
-    it "increments a key with day-granularity in Redis" do
-      expect { post "/track/test-incr" }.to change {
-        Duckweed.redis.get("duckweed:test-incr:days:#{@now.to_i / 86400}")
-      }.from(nil).to(1)
+      it "increments a key with minute-granularity in Redis" do
+        expect { post "/track/#{event}" }.to change {
+          Duckweed.redis.get("duckweed:#{event}:minutes:#{@now.to_i / 60}")
+        }.by(1)
+      end
+
+      it "increments a key with hour-granularity in Redis" do
+        expect { post "/track/#{event}" }.to change {
+          Duckweed.redis.get("duckweed:#{event}:hours:#{@now.to_i / 3600}")
+        }.by(1)
+      end
+
+      it "increments a key with day-granularity in Redis" do
+        expect { post "/track/#{event}" }.to change {
+          Duckweed.redis.get("duckweed:#{event}:days:#{@now.to_i / 86400}")
+        }.by(1)
+      end
     end
   end
 end
