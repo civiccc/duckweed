f1b080441042b409eefed9490463fe1534aa6b3e
Greg Hurrell <greg@causes.com>
2011-05-05 19:24:43 -0700

    Record counters with three different levels of granularity
    The most recent data we want to keep with a per-minute resultion, but we
    only want to keep that data around for a single day.
    
    Older data we only need to keep with a resolution of a day; that is,
    when looking at data from 12 months ago we aren't going to be analyzing
    at the level of minutes and hours but rather days.
    
    Rather than keeping a single counter when recording an event, and then
    having to worry about migrating it to the longer-term, lower-resolution
    data store, we just store all three levels of granularity at once, but
    using different identifying keys and different expiries.

--- lib/duckweed/app.rb
+++ lib/duckweed/app.rb
@@ -4,7 +4,7 @@ module Duckweed
   class App < Sinatra::Base
 
     post "/track/:event" do
-      redis.incr(key_for(params[:event]))
+      increment_counters_for(params[:event])
       "OK"
     end
 
@@ -14,13 +14,41 @@ module Duckweed
 
     private
 
-    def key_for(event, time = Time.now)
-      "duckweed:#{event}:#{time.to_i}"
-    end
-
     def redis
       Duckweed.redis
     end
 
+    INTERVAL = {
+      :minutes => {
+        :bucket_size  => 60,
+        :expiry       => 86400        # keep minute-resolution data for last day
+      },
+      :hours => {
+        :bucket_size  => 3600,
+        :expiry       => 86400 * 7    # keep hour-resolution data for last week
+      },
+      :days => {
+        :bucket_size  => 86400,
+        :expiry       => 86400 * 365  # keep day-resolution data for last year
+      }
+    }
+
+    def increment_counters_for(event)
+      INTERVAL.keys.each do |granularity|
+        key = key_for(event, granularity)
+        redis.incr(key)
+        redis.expire(key, INTERVAL[granularity][:expiry])
+      end
+    end
+
+    def key_for(event, granularity)
+      bucket = bucket_with_granularity(granularity)
+      "duckweed:#{event}:#{bucket}"
+    end
+
+    def bucket_with_granularity(granularity)
+      bucket_idx = Time.now.to_i / INTERVAL[granularity][:bucket_size]
+      "#{granularity}:#{bucket_idx}"
+    end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,6 +11,11 @@ describe Duckweed::App do
   end
 
   describe "POST /track/:event" do
+    before do
+      @now = Time.now
+      Time.stub!(:now).and_return(@now)
+    end
+
     it "accepts requests for new events" do
       post "/track/test-event-71339"
       last_response.should be_successful
@@ -23,16 +28,28 @@ describe Duckweed::App do
       last_response.body.should == "OK"
     end
 
-    it "increments a key in Redis" do
-      now = Time.now
-      Time.stub!(:now).and_return(now)
+    it "increments a key with minute-granularity in Redis" do
+      post "/track/test-incr"
+      Duckweed.redis.get("duckweed:test-incr:minutes:#{@now.to_i / 60}").should == 1
 
       post "/track/test-incr"
-      Duckweed.redis.get("duckweed:test-incr:#{now.to_i}").should == 1
-
-      post "/track/test-incr"
-      Duckweed.redis.get("duckweed:test-incr:#{now.to_i}").should == 2
+      Duckweed.redis.get("duckweed:test-incr:minutes:#{@now.to_i / 60}").should == 2
     end
 
+    it "increments a key with hour-granularity in Redis" do
+      post "/track/test-incr"
+      Duckweed.redis.get("duckweed:test-incr:hours:#{@now.to_i / 3600}").should == 1
+
+      post "/track/test-incr"
+      Duckweed.redis.get("duckweed:test-incr:hours:#{@now.to_i / 3600}").should == 2
+    end
+
+    it "increments a key with day-granularity in Redis" do
+      post "/track/test-incr"
+      Duckweed.redis.get("duckweed:test-incr:days:#{@now.to_i / 86400}").should == 1
+
+      post "/track/test-incr"
+      Duckweed.redis.get("duckweed:test-incr:days:#{@now.to_i / 86400}").should == 2
+    end
   end
 end
