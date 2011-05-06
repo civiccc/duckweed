a97ce4c8f2dd5c78617778ba575203fbb6046789
Greg Hurrell <greg@causes.com>
2011-05-05 19:14:55 -0700

    Add spec helpers to open page in web browser for debugging
    Obvious caveat: this will only work if you are running the tests on your
    local machine, and particularly, it depends on Mac OS X (for the "open"
    command).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,19 @@ require 'mock_redis'
 $LOAD_PATH.unshift(File.expand_path(File.join(__FILE__, "..", "..", "lib")))
 require 'duckweed'
 
+module SpecHelpers
+  def open_page_in_browser
+    path = "/tmp/duckweed_sample_#{$$}.html"
+    File.open(path, 'w') do |f|
+      f.write(last_response.body)
+    end
+    `which open && open #{path}`
+  end
+end
+
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
+  conf.include SpecHelpers
 
   conf.before(:all) do
     Duckweed.redis = MockRedis.new
@@ -16,5 +27,4 @@ RSpec.configure do |conf|
   conf.before(:each) do
     Duckweed.redis.reset!
   end
-
 end
