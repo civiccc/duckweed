186a9fa22f1d3ead4a498aad85fe749b884a21ee
Greg Hurrell <greg@causes.com>
2011-05-06 09:30:00 -0700

    Require an authentication token in order to post a track event
    As stated in the comment, the intention here isn't to prevent unauthorized
    access, but to prevent accidental access, so for that purpose a simple hard-coded
    auth token should suffice.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,37 +12,55 @@ describe Duckweed::App do
 
   describe "POST /track/:event" do
     let(:event) { 'test-event-47781' }
+    let(:default_params) { {:auth_token => Duckweed::AUTH_TOKEN} }
 
     before do
       @now = Time.now
       Time.stub!(:now).and_return(@now)
     end
 
+    context 'without an authentication token' do
+      it 'fails' do
+        post "/track/#{event}", {}
+        last_response.should_not be_successful
+      end
+
+      it 'returns a 403 status code' do
+        post "/track/#{event}", {}
+        last_response.status.should == 403
+      end
+
+      it 'responds with "FORBIDDEN"' do
+        post "/track/#{event}", {}
+        last_response.body.should == 'FORBIDDEN'
+      end
+    end
+
     context 'with a new event' do
       it 'succeeds' do
-        post "/track/#{event}"
+        post "/track/#{event}", default_params
         last_response.should be_successful
       end
 
       it 'responds with "OK"' do
-        post "/track/#{event}"
+        post "/track/#{event}", default_params
         last_response.body.should == "OK"
       end
 
       it "increments a key with minute-granularity in Redis" do
-        expect { post "/track/#{event}" }.to change {
+        expect { post "/track/#{event}", default_params }.to change {
           Duckweed.redis.get("duckweed:#{event}:minutes:#{@now.to_i / 60}")
         }.from(nil).to(1)
       end
 
       it "increments a key with hour-granularity in Redis" do
-        expect { post "/track/#{event}" }.to change {
+        expect { post "/track/#{event}", default_params }.to change {
           Duckweed.redis.get("duckweed:#{event}:hours:#{@now.to_i / 3600}")
         }.from(nil).to(1)
       end
 
       it "increments a key with day-granularity in Redis" do
-        expect { post "/track/#{event}" }.to change {
+        expect { post "/track/#{event}", default_params }.to change {
           Duckweed.redis.get("duckweed:#{event}:days:#{@now.to_i / 86400}")
         }.from(nil).to(1)
       end
@@ -50,33 +68,33 @@ describe Duckweed::App do
 
     context 'with a previously-seen event' do
       before do
-        post "/track/#{event}"
+        post "/track/#{event}", default_params
       end
 
       it 'succeeds' do
-        post "/track/#{event}"
+        post "/track/#{event}", default_params
         last_response.should be_successful
       end
 
       it 'responds with "OK"' do
-        post "/track/#{event}"
+        post "/track/#{event}", default_params
         last_response.body.should == "OK"
       end
 
       it "increments a key with minute-granularity in Redis" do
-        expect { post "/track/#{event}" }.to change {
+        expect { post "/track/#{event}", default_params }.to change {
           Duckweed.redis.get("duckweed:#{event}:minutes:#{@now.to_i / 60}")
         }.by(1)
       end
 
       it "increments a key with hour-granularity in Redis" do
-        expect { post "/track/#{event}" }.to change {
+        expect { post "/track/#{event}", default_params }.to change {
           Duckweed.redis.get("duckweed:#{event}:hours:#{@now.to_i / 3600}")
         }.by(1)
       end
 
       it "increments a key with day-granularity in Redis" do
-        expect { post "/track/#{event}" }.to change {
+        expect { post "/track/#{event}", default_params }.to change {
           Duckweed.redis.get("duckweed:#{event}:days:#{@now.to_i / 86400}")
         }.by(1)
       end
@@ -95,7 +113,7 @@ describe Duckweed::App do
           with("duckweed:#{event}:hours:#{@timestamp / 3600}")
         Duckweed.redis.should_receive(:incr).
           with("duckweed:#{event}:days:#{@timestamp / 86400}")
-        post "/track/#{event}", :timestamp => @timestamp
+        post "/track/#{event}", default_params.merge(:timestamp => @timestamp)
       end
     end
 
@@ -103,21 +121,21 @@ describe Duckweed::App do
       Duckweed.redis.stub!(:expire)
       Duckweed.redis.should_receive(:expire).
         with("duckweed:#{event}:minutes:#{@now.to_i / 60}", 86400)
-      post "/track/#{event}"
+      post "/track/#{event}", default_params
     end
 
     it 'expires hour-granularity data after a week' do
       Duckweed.redis.stub!(:expire)
       Duckweed.redis.should_receive(:expire).
         with("duckweed:#{event}:hours:#{@now.to_i / 3600}", 86400 * 7)
-      post "/track/#{event}"
+      post "/track/#{event}", default_params
     end
 
     it 'expires day-granularity data after a year' do
       Duckweed.redis.stub!(:expire)
       Duckweed.redis.should_receive(:expire).
         with("duckweed:#{event}:days:#{@now.to_i / 86400}", 86400 * 365)
-      post "/track/#{event}"
+      post "/track/#{event}", default_params
     end
   end
 end
