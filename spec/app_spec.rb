784be24f044a24fa743ed1b77b12bbbbcb3b43ab
Greg Hurrell <greg@causes.com>
2011-05-09 22:45:41 -0700

    Add specs for authentication on histogram action

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -337,5 +337,85 @@ describe Duckweed::App do
         last_response.body.should =~ /forbidden/i
       end
     end
+
+    context 'with an authentication token' do
+      before do
+        authorize Duckweed::AUTH_TOKEN, ''
+      end
+
+      context 'with an unknown granularity' do
+        it 'fails' do
+          get "/histogram/#{event}/femtoseconds/10"
+          last_response.should_not be_successful
+        end
+
+        it 'returns a 400 status code' do
+          get "/histogram/#{event}/femtoseconds/10"
+          last_response.status.should == 400
+        end
+
+        it 'responds with "Bad Request"' do
+          get "/histogram/#{event}/femtoseconds/10"
+          last_response.body.should =~ /bad request/i
+        end
+      end
+
+      context 'with minutes granularity' do
+        context 'with a quantity that exceeds the expiry limit' do
+          it 'fails' do
+            get "/histogram/#{event}/minutes/1500" # 1500 minutes = 1 day, 1 hour
+            last_response.should_not be_successful
+          end
+
+          it 'returns a 413 status code' do
+            get "/histogram/#{event}/minutes/1500" # 1500 minutes = 1 day, 1 hour
+            last_response.status.should == 413
+          end
+
+          it 'responds with "Request Entity Too Large"' do
+            get "/histogram/#{event}/minutes/1500" # 1500 minutes = 1 day, 1 hour
+            last_response.body.should =~ /request entity too large/i
+          end
+        end
+      end
+
+      context 'with hours granularity' do
+        context 'with a quantity that exceeds the expiry limit' do
+          it 'fails' do
+            get "/histogram/#{event}/hours/192" # 192 hours = 8 days
+            last_response.should_not be_successful
+          end
+
+          it 'returns a 413 status code' do
+            get "/histogram/#{event}/hours/192"
+            last_response.status.should == 413
+          end
+
+          it 'responds with "Request Entity Too Large"' do
+            get "/histogram/#{event}/hours/192"
+            last_response.body.should =~ /request entity too large/i
+          end
+        end
+      end
+
+      context 'with days granularity' do
+        context 'with a quantity that exceeds the expiry limit' do
+          it 'fails' do
+            get "/histogram/#{event}/days/400" # 400 days = 1 year, 35 days
+            last_response.should_not be_successful
+          end
+
+          it 'returns a 413 status code' do
+            get "/histogram/#{event}/days/400"
+            last_response.status.should == 413
+          end
+
+          it 'responds with "Request Entity Too Large"' do
+            get "/histogram/#{event}/days/400"
+            last_response.body.should =~ /request entity too large/i
+          end
+        end
+      end
+    end
   end
 end
