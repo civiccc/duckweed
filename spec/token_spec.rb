65cc1cca30603020830acb2741462f4e20ba8203
Samuel Merritt <spam@andcheese.org>
2011-07-18 15:26:13 -0700

    Use actual class name instead of described_class for clarity.

--- spec/token_spec.rb
+++ spec/token_spec.rb
@@ -3,34 +3,34 @@ require 'spec_helper'
 describe Duckweed::Token do
   context ".authorize(token, perms)" do
     it "returns token" do
-      described_class.authorize('foo', 'rw').should == 'foo'
+      Duckweed::Token.authorize('foo', 'rw').should == 'foo'
     end
 
     it "makes .authorized?(token, p) true for p in perms" do
       token = "salty-sea-dogs"
 
-      described_class.authorized?(token, 'r').should be_false
-      described_class.authorized?(token, 'w').should be_false
-      described_class.authorize(token, 'rw')
-      described_class.authorized?(token, 'r').should be_true
-      described_class.authorized?(token, 'w').should be_true
+      Duckweed::Token.authorized?(token, 'r').should be_false
+      Duckweed::Token.authorized?(token, 'w').should be_false
+      Duckweed::Token.authorize(token, 'rw')
+      Duckweed::Token.authorized?(token, 'r').should be_true
+      Duckweed::Token.authorized?(token, 'w').should be_true
     end
 
     it "does not affect .authorized?(token, p) for p not in perms" do
       token = "pieces-of-eight"
-      described_class.authorize(token, 'rw')
-      described_class.authorized?(token, 'x').should be_false
+      Duckweed::Token.authorize(token, 'rw')
+      Duckweed::Token.authorized?(token, 'x').should be_false
     end
 
     it "raises an error on nil" do
       lambda do
-        described_class.authorize(nil)
+        Duckweed::Token.authorize(nil)
       end.should raise_error(ArgumentError)
     end
 
     it "raises an error on empty string" do
       lambda do
-        described_class.authorize("")
+        Duckweed::Token.authorize("")
       end.should raise_error(ArgumentError)
     end
   end
@@ -38,11 +38,11 @@ describe Duckweed::Token do
   context ".deauthorize(token)" do
     it "makes .authorized?(token, X) false" do
       token = "scurvy-cur"
-      described_class.authorize(token, 'p')
-      described_class.authorized?(token, 'p').should be_true
+      Duckweed::Token.authorize(token, 'p')
+      Duckweed::Token.authorized?(token, 'p').should be_true
 
-      described_class.deauthorize(token)
-      described_class.authorized?(token, 'p').should be_false
+      Duckweed::Token.deauthorize(token)
+      Duckweed::Token.authorized?(token, 'p').should be_false
     end
   end
 end 
